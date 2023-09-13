17d0c2ce124594cfc5eaedfbeaf3ebd02a7ca6b1
Anderson Loiola Lima <loyolaandersom2013@gmail.com>
2023-09-12 23:39:50 -0300

    feat!: initial swagger configuration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express from "express";
+import swagger from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerDocs from "./swagger.json";
 
 const app = express();
+const swaggerOptions = {
+  swaggerOptions: {
+    validatorUrl: null,
+  },
+};
 
 app.use(express.json());
-
+app.use("/api-docs", swagger.serve, swagger.setup(swaggerDocs, swaggerOptions));
 app.use("/users", usersRoutes);
 
 export { app };
